b5b40717f53c8fd212e732ac1325068445ccc6bb
jkarteen <jasv5_2001@hotmail.com>
2021-12-02 10:49:24 -0500

    footer set and styled

--- app/javascript/react/components/Footer.js
+++ app/javascript/react/components/Footer.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
+import creatorImage from '../../../assets/images/jk2.jpeg'
+
 const Footer = props => {
   return (
     <footer>
       <div className="grid-container" id="footer">
-        <div className="grid-x footer-links">
-          <a className="repo-link" href="https://github.com/jkarteen/jaypardy-trivia">Github</a>
-          <a className="repo-link" href="https://github.com/jkarteen/jaypardy-trivia">Heroku</a>
-          <a className="repo-link" href="https://www.linkedin.com/in/jason-korwek/">LinkedIn</a>
+        <div className="grid-x">
+          <a className="footer-link" href="https://github.com/jkarteen/jaypardy-trivia">Github</a>
+          <a className="footer-link" href="https://www.linkedin.com/in/jason-korwek/">LinkedIn</a>
+          <h4 id="footer-creator">Created by Jason Korwek</h4>
+          <img src={creatorImage} id="photo"></img>
         </div>
       </div>
 
